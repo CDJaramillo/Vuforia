4382f850efaf08e8fe1f2f0984c19a0c55b5e826
Cristian <cdjaramillo2192@gmail.com>
2018-06-20 14:28:36 -0500

    ...
    ...

---
+++ Assets/Scripts/Salir.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+using System;
+public class Salir : MonoBehaviour
+{	public void Update()
+	{
+		if (Application.platform == RuntimePlatform.Android)
+		{
+			if(Input.GetKeyUp (KeyCode.Escape))
+			{
+				Application.Quit();
+				return;
+			}
+		}
+	}
+}
